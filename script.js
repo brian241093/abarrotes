a14a98afa159b511cca1fd429f436046d4c43e20
brian241093 <b45514766@gmail.com>
2025-07-18 17:58:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function agregarAlCarrito(producto, precio) {
+  alert(`Has agregado "${producto}" al carrito por $${precio}`);
+}
+
+function comprarAhora(producto, precio) {
+  alert(`Estás comprando "${producto}" por $${precio}. ¡Gracias por tu compra!`);
+}
